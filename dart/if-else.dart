aca5ac42e277328cd1db86f580c355a7cf007914
RafatMeraz <razhossain201@gmail.com>
2023-05-02 23:54:11 +0600

    basic if else code added

---
+++ dart/if-else.dart
@@ -0,0 +1,14 @@
+main() {
+  int myAge = 10;
+  /// IF-ELSE Condition
+  if (myAge > 25) {
+    print('Job dibo');
+  } else if (myAge > 21) {
+    print('Line a rakho');
+  } else if (myAge > 18) {
+    print('PM er kase pathiye dao');
+  } else {
+    print('job dibo nah');
+  }
+  print('End of program');
+}
