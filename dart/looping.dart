b9c3f7713d398acfec1b3f9d578a0cb1c3bc16b5
RafatMeraz <razhossain201@gmail.com>
2023-05-04 23:28:53 +0600

    looping code added

---
+++ dart/looping.dart
@@ -0,0 +1,28 @@
+main() {
+  // kotobar er jonno jabo?
+  // count koto theke shuru korbo?
+  // jete ashle koto bar count barabe?
+  //
+
+  // starting(once) ; condition ; increment/decrement
+  for (int start = 0; start <= 100; start++) {
+    // niche jabe
+    print('niche jabe $start');
+    print('Kaj korlam');
+  }
+  String name = 'Rahim';
+  int age = 34;
+  String information = 'Name: $name, Age: $age'; // String interpolation
+  print(information);
+  List<String> students = ['Rafat', 'Mehedi', 'Iram'];
+
+  for (int index = 0; index < students.length; index = index + 1) {
+    print('Good morning ${students[index]} ' + students[index]);
+  }
+
+  for (String studentName in students) {
+    print(studentName);
+  }
+
+  /// while / do -- while
+}
