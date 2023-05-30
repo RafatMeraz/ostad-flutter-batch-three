0ad57111123d9b11f4b7c60f4cd83dee7524fe6c
RafatMeraz <razhossain201@gmail.com>
2023-05-30 23:40:37 +0600

    initial hello world project added

---
+++ hello_world/android/app/src/main/kotlin/com/ostad/hello_world/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ostad.hello_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hello_world/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello world',),
+      ),
+    ),
+  );
+}
