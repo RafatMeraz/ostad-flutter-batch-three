b0e97dda311b22d2a7176f868c47a9cca998a4ca
RafatMeraz <razhossain201@gmail.com>
2023-06-06 23:38:51 +0600

    module 6 live class 1 code has been added

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// Container, Button, Icon, image, fab, bottom tab/app
+// FAB, ELEVATED, Outline, textbutton, icon button, inkwell, gesture detector
 
 void main() {
   runApp(MyApp());
@@ -31,66 +31,132 @@ class HomeScreen extends StatelessWidget {
         title: Text('Basic Widgets'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.pink,
-              // margin: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              // padding: EdgeInsets.all(8),
-              // alignment: Alignment.center,
-              padding: EdgeInsets.symmetric(horizontal: 100, vertical: 16),
-              child: Text('Ostad', style: TextStyle(
-                color: Colors.white,
-                fontSize: 24
-              ),),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.amber, width: 4),
-                  borderRadius: BorderRadius.circular(10)),
-              margin: EdgeInsets.only(bottom: 16, top: 10, left: 4, right: 8),
-              alignment: Alignment.center,
-              child: Text(
-                'Ostad',
-                style: TextStyle(color: Colors.white, fontSize: 24),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              /// elevated
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.pink,
+                      // minimumSize: Size(100, 40),
+                      // maximumSize: Size(1000, 40),
+                      elevation: 5,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+                      shadowColor: Colors.deepPurpleAccent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          side: BorderSide(
+                            color: Colors.purple,
+                            width: 3,
+                          ))),
+                  onPressed: () {},
+                  child: Text('Press')),
+              // text button
+              TextButton(
+                style: TextButton.styleFrom(),
+                onPressed: () {},
+                child: Text('Text button'),
               ),
-            ),
-            Icon(
-              Icons.account_circle_outlined,
-              color: Colors.purple,
-              size: 60,
-            ),
-            // https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg
-            Image.network(
-              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-              width: 200,
-              height: 100,
-              fit: BoxFit.cover,
-            ),
-            Container(
-                margin: EdgeInsets.only(top: 16),
-                child: Image.asset('images/owl-two.jpg', width: 100, height: 100,),),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueGrey,
-                  padding: EdgeInsets.symmetric(horizontal: 100, vertical: 16)),
-              onPressed: () {
-                print('hello');
-              },
-              child: Text(
-                'Add',
-                style: TextStyle(fontSize: 24),
+              // outline button
+              OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.blue)),
+                  onPressed: () {},
+                  child: Text('Outlined button')),
+              // outline button with icon
+              OutlinedButton.icon(
+                  style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.blue)),
+                  onPressed: () {},
+                  icon: Icon(Icons.account_tree),
+                  label: Text('Outlined button with icon')),
+              // icon button
+              IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+              // gesture detector
+              GestureDetector(
+                  onLongPress: () {
+                    print('long press');
+                  },
+                  onDoubleTap: () {
+                    print('double pressed');
+                  },
+                  onTap: () {
+                    print('Pressed');
+                  },
+                  child: Image.asset(
+                    'images/owl-two.jpg',
+                    width: 100,
+                  )),
+              // ink well
+              InkWell(
+                splashColor: Colors.yellow,
+                onLongPress: () {
+                  print('long press');
+                },
+                onDoubleTap: () {
+                  print('double pressed');
+                },
+                onTap: () {
+                  print('Pressed');
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('sdfdf'),
+                ),
               ),
-            )
-          ],
+              Card(
+                elevation: 5,
+                shadowColor: Colors.purple,
+                color: Colors.blueGrey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text('MD Faysal Ahmed'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your email',
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 2)),
+                  ),
+                ),
+              ),
+              TextField(
+                controller: TextEditingController(),
+                obscureText: true,
+                onSubmitted: (value) {
+                  print(value);
+                },
+                onChanged: (value) {
+                  // print(value);
+                },
+                textInputAction: TextInputAction.done,
+                // readOnly: true,
+                textAlign: TextAlign.center,
+              ),
+              TextField(),
+              TextField(),
+              TextField(),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
