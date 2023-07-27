aa5fc881d89bee63db8ccf96fe2aadcd77f75b28
RafatMeraz <razhossain201@gmail.com>
2023-07-27 22:37:23 +0600

    login api integration has been done

--- intro_widget/lib/data/utils/urls.dart
+++ intro_widget/lib/data/utils/urls.dart
@@ -3,4 +3,5 @@ class Urls {
 
   static const String _baseUrl = 'https://task.teamrabbil.com/api/v1';
   static String registration = '$_baseUrl/registration';
+  static String login = '$_baseUrl/login';
 }
--- intro_widget/lib/ui/screens/auth/login_screen.dart
+++ intro_widget/lib/ui/screens/auth/login_screen.dart
@@ -1,97 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:intro_widget/data/models/network_response.dart';
+import 'package:intro_widget/data/services/network_caller.dart';
+import 'package:intro_widget/data/utils/urls.dart';
 import 'package:intro_widget/ui/screens/bottom_nav_base_screen.dart';
 import 'package:intro_widget/ui/screens/email_verification_screen.dart';
 import 'package:intro_widget/ui/screens/auth/signup_screen.dart';
 import 'package:intro_widget/ui/widgets/screen_background.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+
+  bool _loginInProgress = false;
+
+  Future<void> login() async {
+    _loginInProgress = true;
+    if (mounted) {
+      setState(() {});
+    }
+    Map<String, dynamic> requestBody = {
+      "email": _emailTEController.text.trim(),
+      "password": _passwordTEController.text
+    };
+    final NetworkResponse response =
+        await NetworkCaller().postRequest(Urls.login, requestBody);
+    _loginInProgress = false;
+    if (mounted) {
+      setState(() {});
+    }
+    if (response.isSuccess) {
+      if (mounted) {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const BottomNavBaseScreen()),
+            (route) => false);
+      }
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Incorrect email or password')));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: ScreenBackground(
           child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 64,),
-                  Text(
-                    'Get Started With',
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .titleLarge,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              Text(
+                'Get Started With',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextField(
+                controller: _emailTEController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              TextField(
+                controller: _passwordTEController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Visibility(
+                  visible: _loginInProgress == false,
+                  replacement: const Center(
+                    child: CircularProgressIndicator(),
                   ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  const TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushAndRemoveUntil(
-                              context, MaterialPageRoute(builder: (
-                              context) => const BottomNavBaseScreen()), (
-                              route) => false);
-                        },
-                        child: const Icon(Icons.arrow_forward_ios)),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Center(
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (
-                            context) => const EmailVerificationScreen()));
+                  child: ElevatedButton(
+                      onPressed: () {
+                        login();
                       },
-                      child: const Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ),
+                      child: const Icon(Icons.arrow_forward_ios)),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Center(
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const EmailVerificationScreen()));
+                  },
+                  child: const Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.grey),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Don't have an account?",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, letterSpacing: 0.5),
-                      ),
-                      TextButton(onPressed: () {
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account?",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500, letterSpacing: 0.5),
+                  ),
+                  TextButton(
+                      onPressed: () {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
                                 builder: (context) => const SignUpScreen()));
-                      }, child: const Text('Sign up')),
-                    ],
-                  )
+                      },
+                      child: const Text('Sign up')),
                 ],
-              ),
-            ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    ));
   }
 }
