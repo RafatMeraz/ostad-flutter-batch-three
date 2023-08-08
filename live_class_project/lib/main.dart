9a92249a2a3cead9bcd67d26dc07cb8477abfe98
RafatMeraz <razhossain201@gmail.com>
2023-08-08 23:51:23 +0600

    intro to getx code pushed

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
-// package import
-import 'package:live_class_project/home.dart';
-// relative import
-import 'home.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyCounterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyCounterApp extends StatelessWidget {
+  const MyCounterApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return GetMaterialApp(
       home: HomeScreen(),
     );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final CounterController counterController = CounterController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Obx(
+          () => Text(
+            counterController.count.toString(),
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counterController.increment();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CounterController extends GetxController {
+  RxInt count = 0.obs;
+
+  void increment() {
+    count++;
   }
 }
